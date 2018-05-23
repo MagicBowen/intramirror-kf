7caa63f78d1f3be1a286f0b878c04f803541e3bd
magicbowen <e.bowen.wang@icloud.com>
2018-05-24 00:40:36 +0800

    add  more

--- controllers/index.js
+++ controllers/index.js
@@ -1,16 +1,21 @@
 const logger = require('../logger').logger('index');
 const chatbot = require('../domain/chatbot')
+const fs = require('mz/fs');
 
 const MESSAGE_TEXT_TYPE = 0;
 const REQUEST_TALK_MESSAGE_TYPE = 1;
 
-var receiveMsg = async (ctx, next) => {
+
+var handleMsg = async (ctx, next) => {
     const msg = ctx.request.body;
     if (msg.request_type === REQUEST_TALK_MESSAGE_TYPE ) {
         if (msg.message_type === MESSAGE_TEXT_TYPE) {
+            logger.debug(msg);
             const text = msg.text;
             logger.info(`Receive msg : ${text}`);
-            chatbot.send('sendTextMessage', {chat_id : msg.chat_id, chat_type : msg.chat_type, text : text});
+            const file = `${__dirname}/../static/image/01.png`;
+            const image = fs.createReadStream(file);
+            chatbot.send('sendImageMessage', {chat_id : msg.chat_id, chat_type : msg.chat_type, text : text, image : image});
         }
     }
 }
@@ -21,5 +26,5 @@ var index = async (ctx, next) => {
 
 module.exports = {
     'GET /'  : index,
-    'POST /' : receiveMsg
+    'POST /' : handleMsg
 };
--- utils/syncpost.js
+++ utils/syncpost.js
@@ -1,19 +1,20 @@
-const requestJson = require('request-json');
+// const requestJson = require('request-json');
 
-function syncPost(uri, data) {
-    var client = requestJson.createClient(uri);
-    return new Promise(function (resolve, reject) {
-        client.post('query', data, function (error, res, body) {
-        if (!error && res.statusCode == 200) {
-          resolve(body.reply);
-        } else {
-          reject(error);
-        }
-      });
-    });
-}
+// function syncPost(uri, data) {
+//     var client = requestJson.createClient(uri);
+//     return new Promise(function (resolve, reject) {
+//         client.post('query', data, function (error, res, body) {
+//         if (!error && res.statusCode == 200) {
+//           resolve(body.reply);
+//         } else {
+//           reject(error);
+//         }
+//       });
+//     });
+// }
 
-const request = require('request')
+const request = require('request');
+var FormData = require('form-data');
 
 function syncGet(uri) {
   return new Promise( (resolve, reject) => {
@@ -26,6 +27,34 @@ function syncGet(uri) {
       })
   });
 }
+
+function syncPost(uri, data) {
+    return new Promise( (resolve, reject) => {
+      request.post({url : uri, formData : data })
+      .on('response', function(response) {
+        resolve(response);
+      })
+      .on('error', function(error) {
+        reject(error);
+      })
+  });
+}
+
+// function syncPost(uri, data, file) {
+//   var form = new FormData(data);
+//   return new Promise( (resolve, reject) => {
+//     request.post(uri)
+//     .on('response', function(response) {
+//       if (file) {
+//         form.append('image', fs.createReadStream(file));
+//       }
+//       resolve(response);
+//     })
+//     .on('error', function(error) {
+//       reject(error);
+//     })
+// });
+// }
 
 module.exports.syncPost = syncPost;
 module.exports.syncGet = syncGet;
