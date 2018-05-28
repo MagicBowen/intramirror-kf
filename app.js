63b71cefc936e992e0eefa1ea0ad60fcd924b644
magicbowen <e.bowen.wang@icloud.com>
2018-05-28 17:53:16 +0800

    add more

--- app.js
+++ app.js
@@ -232,7 +232,7 @@ bot.hears(/.+/, (ctx) => {
     }
     let message = ctx.message.reply_to_message.text || ctx.message.reply_to_message.caption
     if (message.split(':').length < 2) {
-        return ctx.reply('Do not reply my message😆');
+        return ctx.reply('Do not reply my message [face](https://www.magicbowen.top/big.png)', Extra.markdown().webPreview());
     }
 
     let toName = ctx.message.reply_to_message.text.split(':')[0];
