0e88368957ca7babf5e9839190ad5612ea624a8d
magicbowen <e.bowen.wang@icloud.com>
2018-05-29 10:19:43 +0800

    add more

--- app.js
+++ app.js
@@ -228,11 +228,11 @@ bot.hears(/.+/, (ctx) => {
         return ctx.reply('You have not registered, please register first!');    
     }
     if (!ctx.message.reply_to_message) {
-        return ctx.reply('You send a message with no receiver☹️, [help](https://www.magicbowen.top)!', Extra.markdown().webPreview());
+        return ctx.reply('You send a message with no receiver [help](https://www.magicbowen.top)!', Extra.markdown().webPreview());
     }
     let message = ctx.message.reply_to_message.text || ctx.message.reply_to_message.caption;
     if (message.split(':').length < 2) {
-        return ctx.reply('Do not reply my message [face](https://www.magicbowen.top/small.png)', Extra.markdown().webPreview());
+        return ctx.reply('Do not reply my message☹️!');
     }
 
     let toName = message.split(':')[0];
